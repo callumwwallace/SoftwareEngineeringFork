71d898833173379e76bf26ddfc9c4a28c2c9383b
callumwwallace <145865304+callumwwallace@users.noreply.github.com>
2023-10-19 16:11:27 +0100

    Week 1
    Complete

--- Tasks/01 - Variables and Arithmetic/121-scanf/121-scanf.cpp
+++ Tasks/01 - Variables and Arithmetic/121-scanf/121-scanf.cpp
@@ -4,18 +4,25 @@ int main()
 {
     //Create a variable x
     int x = 0;
-
+    int y = 0;
     //Prompt user
     printf("Enter your age\n");
 
     //Read keyboard response - look for an integer
     int found = scanf_s("%d", &x);
 
+    printf("Enter your age\n");
+
+    //Read keyboard response - look for an integer
+    int found2 = scanf_s("%d", &y);
+
     //Display how many items were matched
-    printf("Matched %d items\n", found);
+    printf("Matched %d items input1\n", found + found2);
 
     //Display what was typed in
     printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("avg = %d\n", ((x + y) / (found + found2)));
 }
 
 
